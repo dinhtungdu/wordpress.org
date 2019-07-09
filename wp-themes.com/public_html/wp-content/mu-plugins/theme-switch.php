9d902d7a013896071af35f0a70ab325b7a03c5e9
Dion Hulse <dd32@dd32.id.au>
2019-07-09 16:34:48 +1000

    When running a development install, attempt to install the Theme upon access.

--- wp-themes.com/public_html/wp-content/mu-plugins/theme-switch.php
+++ wp-themes.com/public_html/wp-content/mu-plugins/theme-switch.php
@@ -30,9 +30,10 @@ class WP_Themes_Theme_Switcher {
 	/**
 	 * Determine the current theme based on the REQUEST_URI, the first component of the URI is the theme slug.
 	 */
-	protected function determine_theme() {
+	protected function determine_theme( $return_stylesheet_early_without_validation = false ) {
 		// If the first component in the URL is a stylesheet, use it and chop it off the REQUEST_URI
-		$stylesheet = array_shift( array_filter( explode( '/', $_SERVER['REQUEST_URI'] ) ) );
+		$parts = array_filter( explode( '/', $_SERVER['REQUEST_URI'] ) );
+		$stylesheet = array_shift( $parts );
 
 		if ( ! $stylesheet ) {
 			return false;
@@ -40,7 +41,12 @@ class WP_Themes_Theme_Switcher {
 
 		// For base-files, trust the referer instead.
 		if ( isset( $_SERVER['HTTP_REFERER'] ) && '.php' == substr( $stylesheet, -4 ) ) {
-			$stylesheet = array_shift( array_filter( explode( '/', parse_url( $_SERVER['HTTP_REFERER'], PHP_URL_PATH ) ) ) );
+			$parts = array_filter( explode( '/', parse_url( $_SERVER['HTTP_REFERER'], PHP_URL_PATH ) ) );
+			$stylesheet = array_shift( $parts );
+		}
+
+		if ( $return_stylesheet_early_without_validation ) {
+			return $stylesheet;
 		}
 
 		$theme = wp_get_theme( $stylesheet );
@@ -57,10 +63,41 @@ class WP_Themes_Theme_Switcher {
 		return $theme;
 	}
 
+	function maybe_install_theme() {
+		if ( $this->theme && $this->theme->exists() ) {
+			return;
+		}
+		if ( ! defined( 'WP_DEBUG' ) || ! WP_DEBUG ) {
+			return;
+		}
+
+		$stylesheet = $this->determine_theme( $return_stylesheet_early_without_validation = true );
+
+		if ( get_site_transient( "failed_theme_install_{$stylesheet}" ) ) {
+			return;
+		}
+
+		// Attempt installation?
+		add_filter( 'filesystem_method', function() { return 'direct'; } );
+		include_once ABSPATH . 'wp-admin/includes/admin.php';
+		include_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
+		$upgrader = new Theme_Upgrader( $skin = new WP_Ajax_Upgrader_Skin() );
+		$result   = $upgrader->install( "https://downloads.wordpress.org/theme/{$stylesheet}.latest-stable.zip" );
+
+		if ( $result ) {
+			$this->theme = $this->determine_theme();
+		} else {
+			set_site_transient( "failed_theme_install_{$stylesheet}", true, DAY_IN_SECONDS );
+		}
+	}
+
 	/**
 	 * Redirect unknown theme paths to the default theme.
 	 */
 	function redirect_unknown_themes() {
+		if ( ! $this->theme && defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+			$this->maybe_install_theme();
+		}
 		if ( ! $this->theme ) {
 			$default_theme = WP_Theme::get_core_default_theme();
 			wp_safe_redirect( home_url( $default_theme->stylesheet . '/' ) );
